256688c7c02042bf68fb3a768017e8a00393060d
aApprendeIdiomas <apprendeIdiomas@gmail.com>
2021-12-19 15:43:22 -0600

    navbar y pagina de inicio look and feel

---
+++ cmp/mi-nav.js
@@ -0,0 +1,32 @@
+class MiNav extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = /* html */
+      `<ul>
+        <li>
+          <a href="index.html">
+            Inicio</a>
+        </li>
+        <li>
+          <a href="speaking.html">
+            Speakin</a>
+        </li>
+        <li>
+          <a href="reading.html">
+            Reading</a>
+        </li>
+        <li>
+          <a href="listening.html">
+            Listening</a>
+        </li>
+        <li>
+          <a href="writing.html">
+            Writing</a>
+        </li>
+      </ul>`;
+    this.ul =
+      this.querySelector("ul");
+  }
+
+}
+
+customElements.define("mi-nav", MiNav);
