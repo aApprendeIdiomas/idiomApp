7b6ae3d25ad5caf614296a604c7678bc7bd73e2e
aApprendeIdiomas <apprendeIdiomas@gmail.com>
2021-12-20 22:25:19 -0600

    look and feel y sesion

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyCf0GzzM0SfyoLrQOmapy_ND_ePUe3IoNk",
+  authDomain: "apprendeidiomas-c7cdc.firebaseapp.com",
+  projectId: "apprendeidiomas-c7cdc",
+  storageBucket: "apprendeidiomas-c7cdc.appspot.com",
+  messagingSenderId: "421492407707",
+  appId: "1:421492407707:web:fe8f34b41009059ef8753e",
+  measurementId: "G-89R9JLGQZK"
+});
